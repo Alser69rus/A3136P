a2e905dae29990eadf7c969fe34b0410edb6c477
Alser69rus <evdokimov69rus@mail.ru>
2019-09-06 15:30:02 +0300

    исправление названия меню регулировки БУ

--- menu/mnuBU.py
+++ menu/mnuBU.py
@@ -4,7 +4,7 @@ from menu.btn import Btn
 
 class MnuBU(Menu):
     def __init__(self, parent=None):
-        super().__init__('Проверка блока управления', parent)
+        super().__init__('Регулировка блока управления', parent)
 
         self.btn_bu_ai_tune = Btn('Настройка АВХ1 - ДДН и АВХ2 - ДДМ')
         self.btn_bu_ai_3_tune = Btn('Настройка АВХ3 - датчик температуры масла\n'
